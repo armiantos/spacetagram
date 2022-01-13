5eff00b112167185fd936892f60a7432697edff6
Armianto Sumitro <armiantos@gmail.com>
2022-01-13 01:41:55 -0700

    Add test to fetch and render loading screen

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import axios from 'axios';
 
-test('renders learn react link', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+import App from './App';
+import { act } from 'react-dom/test-utils';
+
+jest.mock('axios');
+const mockedAxios = jest.mocked(axios, true);
+
+beforeEach(() => {
+    mockedAxios.get.mockResolvedValue({
+        data: [],
+    });
+});
+
+test("it fetches NASA's APOD API", () => {
+    const expected_endpoint = 'https://api.nasa.gov/planetary/apod';
+    const expected_params = {
+        count: 20,
+        api_key: process.env.REACT_APP_NASA_API_KEY,
+    };
+
+    act(() => {
+        render(<App />);
+    });
+
+    expect(mockedAxios.get).toBeCalledWith(expected_endpoint, {
+        params: expected_params,
+    });
+});
+
+test('initially renders a loading screen', () => {
+    act(() => {
+        render(<App />);
+    });
+
+    const loadingTextElement = screen.getByText('Loading');
+    expect(loadingTextElement).toBeInTheDocument();
 });
