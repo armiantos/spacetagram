1bf66c97e280d13ce6d6846a6aec281217f95dd7
Armianto Sumitro <armiantos@gmail.com>
2022-01-13 01:17:38 -0700

    Display 20 Astronomy Pictures of the Day

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+export interface ApodResponse {
+    copyright: string;
+    date: string;
+    explanation: string;
+    hdurl: string;
+    media_type: string;
+    service_version: string;
+    title: string;
+    url: string;
+}
 
 function App() {
+    const [apods, setApods] = useState<ApodResponse[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            const APOD_ENDPOINT = 'https://api.nasa.gov/planetary/apod';
+            const NASA_API_KEY = process.env.REACT_APP_NASA_API_KEY;
+            setIsLoading(true);
+            const response = await axios.get<ApodResponse[]>(APOD_ENDPOINT, {
+                params: {
+                    count: 20,
+                    api_key: NASA_API_KEY,
+                },
+            });
+            setIsLoading(false);
+            setApods(response.data);
+        })();
+    }, []);
+
+    if (isLoading) {
+        return <div>Loading</div>;
+    }
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div>
+            {apods
+                .filter((apod) => apod.media_type === 'image')
+                .map((apod) => (
+                    <img key={apod.url} alt={apod.title} src={apod.url} />
+                ))}
         </div>
     );
 }
