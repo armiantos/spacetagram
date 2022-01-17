de7c47925c09b506b0591d1681f1245f72ed4420
Armianto Sumitro <armiantos@gmail.com>
2022-01-17 15:19:17 -0700

    Delegate fetching to fetchApod thunk

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { browseSlice } from './slices/browseSlice';
 import { likedImagesSlice } from './slices/likedImagesSlice';
 
 export const store = configureStore({
     reducer: {
         likedImages: likedImagesSlice.reducer,
+        browse: browseSlice.reducer,
     },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
---
+++ src/redux/thunks/browse/fetchApod.ts
@@ -0,0 +1,17 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+import { Image } from '../../../data/Image';
+
+export const fetchApod = createAsyncThunk('browse/fetchApod', async () => {
+    const APOD_ENDPOINT = 'https://api.nasa.gov/planetary/apod';
+    const NASA_API_KEY = process.env.REACT_APP_NASA_API_KEY;
+
+    const response = await axios.get<Image[]>(APOD_ENDPOINT, {
+        params: {
+            count: 20,
+            api_key: NASA_API_KEY,
+        },
+    });
+    return response.data;
+});
