d102c2f29f4061b542535696f3be8ea9d2173c9e
Armianto Sumitro <armiantos@gmail.com>
2022-01-18 13:03:13 -0700

    Integrate redux persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 import { browseSlice } from './slices/browseSlice';
 import { focusSlice } from './slices/focusSlice';
 import { likedImagesSlice } from './slices/likedImagesSlice';
 
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const rootReducer = combineReducers({
+    likedImages: likedImagesSlice.reducer,
+    browse: browseSlice.reducer,
+    focus: focusSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
     reducer: {
-        likedImages: likedImagesSlice.reducer,
-        browse: browseSlice.reducer,
-        focus: focusSlice.reducer,
+        persistedReducer,
     },
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
