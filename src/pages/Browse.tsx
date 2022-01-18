610844b1e9e36c261da9ce7861323fa5ed933dd5
Armianto Sumitro <armiantos@gmail.com>
2022-01-17 23:34:19 -0700

    Remove unused imports

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -1,6 +1,5 @@
 import LoadingButton from '@mui/lab/LoadingButton';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import React, { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
