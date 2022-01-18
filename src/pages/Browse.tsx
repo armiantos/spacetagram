a9f472171ea7e5e5938ae4768c4817c983b3d755
Armianto Sumitro <armiantos@gmail.com>
2022-01-17 23:08:17 -0700

    Implement auto fetch next page

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -1,21 +1,51 @@
+import LoadingButton from '@mui/lab/LoadingButton';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import React, { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { fetchApod } from '../redux/thunks/browse/fetchApod';
 import { Loading } from '../shared_components/Loading';
 import { MediaGrid } from '../shared_components/MediaGrid';
+import { theme } from '../theme';
 
 export function Browse() {
     const dispatch = useAppDispatch();
     const apods = useAppSelector((state) => state.browse.images);
     const isLoading = useAppSelector((state) => state.browse.isLoading);
+    const { ref, inView } = useInView();
 
-    const loading = isLoading ? <Loading /> : null;
+    const loading = isLoading && apods.length === 0 ? <Loading /> : null;
 
     useEffect(() => {
-        dispatch(fetchApod());
-    }, []);
+        if (apods.length == 0 || inView) {
+            dispatch(fetchApod());
+        }
+    }, [apods, inView]);
+
+    let loadMore = null;
+    if (apods.length > 0) {
+        loadMore = (
+            <Box
+                sx={{
+                    display: 'grid',
+                    placeItems: 'center',
+                    paddingTop: theme.spacing(4),
+                    paddingBottom: theme.spacing(2),
+                }}
+            >
+                <LoadingButton
+                    variant="contained"
+                    ref={ref}
+                    loading={isLoading}
+                >
+                    Load more
+                </LoadingButton>
+            </Box>
+        );
+    }
 
     return (
         <>
@@ -24,6 +54,7 @@ export function Browse() {
             </Typography>
             {loading}
             <MediaGrid images={apods} />
+            {loadMore}
         </>
     );
 }
--- src/shared_components/Loading.tsx
+++ src/shared_components/Loading.tsx
@@ -2,9 +2,18 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/system/Box';
 import React from 'react';
 
+import { theme } from '../theme';
+
 export function Loading() {
     return (
-        <Box sx={{ maxWidth: '100%', display: 'grid', placeItems: 'center' }}>
+        <Box
+            sx={{
+                maxWidth: '100%',
+                display: 'grid',
+                placeItems: 'center',
+                margin: theme.spacing(3),
+            }}
+        >
             <CircularProgress />
         </Box>
     );
