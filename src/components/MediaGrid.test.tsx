bdbb9134023882dbcacf2f8646f6dc7c5d451af3
Armianto Sumitro <armiantos@gmail.com>
2022-01-18 14:45:46 -0700

    Test for number of columns in MediaGrid

--- src/components/MediaGrid.test.tsx
+++ src/components/MediaGrid.test.tsx
@@ -1,4 +1,5 @@
-import { useMediaQuery } from '@mui/material';
+import { Grid } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { shallow } from 'enzyme';
 import React from 'react';
 
@@ -6,11 +7,46 @@ import { mockImages } from '../testConstants';
 import { MediaCard } from './MediaCard';
 import { MediaGrid } from './MediaGrid';
 
-jest.mocked(useMediaQuery);
+jest.mock('@mui/material/useMediaQuery');
+const mockUseMediaQuery = jest.mocked(useMediaQuery, true);
 
 test('renders images as MediaCards', () => {
     const result = shallow(<MediaGrid images={mockImages} />);
 
     const cards = result.find(MediaCard);
     expect(cards).toHaveLength(mockImages.length);
+});
+
+test('renders the 3 columns for large screens', () => {
+    mockUseMediaQuery.mockReturnValueOnce(false);
+    mockUseMediaQuery.mockReturnValueOnce(true);
+
+    const result = shallow(<MediaGrid images={mockImages} />);
+
+    const columns = result.findWhere(
+        (wrapper) => wrapper.type() === Grid && wrapper.prop('item')
+    );
+    expect(columns).toHaveLength(3);
+});
+
+test('renders the 2 columns for medium screens', () => {
+    mockUseMediaQuery.mockReturnValueOnce(true);
+
+    const result = shallow(<MediaGrid images={mockImages} />);
+
+    const columns = result.findWhere(
+        (wrapper) => wrapper.type() === Grid && wrapper.prop('item')
+    );
+    expect(columns).toHaveLength(2);
+});
+
+test('renders the 1 column for small screens', () => {
+    mockUseMediaQuery.mockReturnValue(false);
+
+    const result = shallow(<MediaGrid images={mockImages} />);
+
+    const columns = result.findWhere(
+        (wrapper) => wrapper.type() === Grid && wrapper.prop('item')
+    );
+    expect(columns).toHaveLength(1);
 });
