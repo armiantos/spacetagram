a9d0b878c81b91ad3bfdda11b06c66775835a56d
Armianto Sumitro <armiantos@gmail.com>
2022-01-17 21:58:03 -0700

    Use dialog instead of unstyled modal

--- src/shared_components/FocusedModal.tsx
+++ src/shared_components/FocusedModal.tsx
@@ -1,33 +1,68 @@
 import React from 'react';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { focusSlice } from '../redux/slices/focusSlice';
-import { DetailedMediaCard } from './DetailedMediaCard';
+import { pink } from '@mui/material/colors';
+import { likedImagesSlice } from '../redux/slices/likedImagesSlice';
+import { Image } from '../data/Image';
+import DialogContent from '@mui/material/DialogContent';
+import Typography from '@mui/material/Typography';
+
+function isLiked(image: Image, likedImages: Image[]) {
+    return likedImages.includes(image);
+}
 
 export function FocusedModal() {
     const dispatch = useAppDispatch();
+    const likedImages = useAppSelector((state) => state.likedImages.images);
     const focusedImage = useAppSelector((state) => state.focus.image);
 
+    const open = focusedImage !== undefined;
+    const handleClose = () => {
+        dispatch(focusSlice.actions.defocus());
+    };
+
+    if (!open) {
+        return null;
+    }
+
+    let favoriteIcon = <FavoriteIcon />;
+    if (isLiked(focusedImage, likedImages)) {
+        favoriteIcon = <FavoriteIcon sx={{ color: pink[500] }} />;
+    }
+
     return (
-        <Modal
-            open={focusedImage !== undefined}
-            onClose={() => {
-                dispatch(focusSlice.actions.defocus());
-            }}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-        >
-            <Box
-                sx={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                }}
-            >
-                <DetailedMediaCard image={focusedImage} />
-            </Box>
-        </Modal>
+        <Dialog onClose={handleClose} open={open} maxWidth="md" scroll="paper">
+            <DialogTitle>{focusedImage.title}</DialogTitle>
+            <img src={focusedImage.url} alt={focusedImage.title} width="100%" />
+            <DialogContent>
+                <Typography variant="body1" gutterBottom>
+                    {focusedImage.date}
+                </Typography>
+                <Typography variant="body2">
+                    {focusedImage.explanation}
+                </Typography>
+            </DialogContent>
+            <DialogActions>
+                <IconButton
+                    aria-label="like photo"
+                    onClick={() => {
+                        dispatch(
+                            likedImagesSlice.actions.toggleLikePhoto(
+                                focusedImage
+                            )
+                        );
+                    }}
+                >
+                    {favoriteIcon}
+                </IconButton>
+            </DialogActions>
+        </Dialog>
     );
 }
